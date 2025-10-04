517130b2ce8898758fd46b1ad3a21275fbb00780
LeHoangQuachTinh <23115053122145@sv.ute.udn.vn>
2025-10-04 12:48:17 +0700

    update screen login and register

--- app/src/main/java/com/example/myapplication/ActivityLayout1.java
+++ app/src/main/java/com/example/myapplication/ActivityLayout1.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,7 +25,7 @@ public class ActivityLayout1 extends AppCompatActivity {
             return insets;
         });
 
-        Button btnRegister= findViewById(R.id.button3_2145);
+        TextView btnRegister= findViewById(R.id.txtRegister);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
